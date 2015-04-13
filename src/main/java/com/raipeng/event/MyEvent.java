2b5114d5a30b640f17c3cd102699ebd11b4ddac6
xflin <xflin@raipeng.com>
2015-04-13 23:00:38 +0800

    事物

---
+++ src/main/java/com/raipeng/event/MyEvent.java
@@ -0,0 +1,14 @@
+package com.raipeng.event;
+
+import java.util.EventObject;
+
+public class MyEvent extends EventObject {
+
+	private static final long serialVersionUID = 7253673262227969369L;
+
+    //父类中的事件源
+	public MyEvent(Object source) {
+		super(source);
+	}
+
+}
